91fd8780e33cfd0b185595d8f0ba593553f6592f
Lulu Shi <crazzy_rabbit@163.com>
2024-07-22 19:01:55 +0800

    Update add_samplesize.py

--- Meta GWAS/add_samplesize.py
+++ Meta GWAS/add_samplesize.py
@@ -20,18 +20,27 @@ from io import StringIO
 @click.option('--samplesize', "-S", help="sample size you want to add", type=int, required=True)
 @click.option('--output', '-O', help="output txt.gz you added samplesize", required=True)
 def main(infile, samplesize, output):
+    """
+    add sample size and change GWAS summary data header to MR-MEGA in file format \n
+    only applicable to my private analysis
+    such: MARKERNAME EA NEA BETA SE EAF N CHR POS
+    """
     if infile.endswith(".gz"):
         with gzip.open(infile, 'rt') as f_in:
             csv_content = f_in.read()   
-        df = pd.read_csv(StringIO(csv_content), sep='\\s+', engine='python')  # 使用StringIO处理非字符串内容
+        df = pd.read_csv(StringIO(csv_content), dtype={'POS':int}, 
+                         sep='\\s+', engine='python')  # 使用StringIO处理非字符串内容
         df['nSample'] = samplesize
         with gzip.open(output, 'wt') as f_out:
-            df.to_csv(f_out, sep='\t', index=False)
-            
+            df[['SNP', 'ALLELE0', 'ALLELE1', 'BETA', 'SE',
+                'A1FREQ', 'nSample', 'CHR', 'BP']].to_csv(f"{f_out}.MRMEGAin.gz", sep='\t', index=False)
+
     else:
-        df = pd.read_csv(infile, sep='\\s+', engine='python')
+        df = pd.read_csv(infile, dtype={'POS':int}, 
+                         sep='\\s+', engine='python')
         df['nSample'] = samplesize
-        df.to_csv(output, sep='\t', index=False)
+        df[['SNP', 'A1', 'A2', 'BETA', 'SE',
+            'A1Frq', 'nSample', 'CHR', 'POS']].to_csv(f"{output}.MRMEGAin", sep='\t', index=False)
 
 if __name__ == '__main__':
     main()
