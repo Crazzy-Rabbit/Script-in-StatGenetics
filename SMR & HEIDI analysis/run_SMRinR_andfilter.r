49a005816a257eccb0dcd21b289f7c6f1e035980
Lulu Shi <crazzy_rabbit@163.com>
2024-10-21 11:11:48 +0800

    Update run_SMRinR_andfilter.r

--- SMR & HEIDI analysis/run_SMRinR_andfilter.r
+++ SMR & HEIDI analysis/run_SMRinR_andfilter.r
@@ -1,7 +1,5 @@
-# # # run_SMRinR_fiter.r
 library(dplyr)
 library(data.table)
-
 # r function run smr qtl to trait
 run_smr_qtl <- function(gwas, qtl, outname){
     SMR="/public/home/lijunpeng/smr-1.3.1-linux-x86_64/smr-1.3.1"
@@ -20,24 +18,15 @@ run_smr_qtl <- function(gwas, qtl, outname){
 # p_SMR & p_HEIDI filter
 run_filter <- function(smr, outname){
     smr_file = fread(smr)
-    flt_file = smr_file %>% filter(p_SMR <= 0.05/nrow(smr_file) & p_HEIDI >= 0.01)
+    flt_file = smr_file %>% filter(p_SMR <= 0.05/n() & p_HEIDI >= 0.01)
 
     write.table(flt_file, file=paste0(outname, "_", sprintf("%.2e", 0.05/nrow(smr_file)), "smr_0.01heidi.txt"), sep="\t", row.names=FALSE, quote=FALSE)
 }
 
-# # # start run smr and filter
-# whole blood eQTL #
-eQTL = "/public/home/gaikai/data/multi_tissue_xQTL/eqtl_all_tissue/Whole_Blood"
-eQTLGen = "/public/home/gaikai/data/multi_tissue_xQTL/eqtl_all_tissue/Whole_Blood"
-gwas = "~/reformatMETAL.gz"
+args <- commandArgs(TRUE)
+gwas <- args[which(args == "--gwas") + 1]
+qtl  <- args[which(args == "--qtl") + 1]
+out  <- args[which(args == "--out")  + 1]
 
-# run smr and filter
-setwd("~/SMR/whole_blood/")
-outprx = paste0(basename(gwas), "_whole_blood")
-
-for (qtl in c(eQTL, eQTLGen)){
-    qtlname = ifelse(grepl("eQTL", qtl), "eQTL", "eQTLGen")
-    outname = paste0(outprx, "_", qtlname)
-    run_smr_qtl(gwas, qtl, outname)
-    run_filter(paste0(outname,".smr"), outname)
-}
+run_smr_qtl(gwas, qtl, out)
+runfilter(paste0(out, ".smr"), out)
