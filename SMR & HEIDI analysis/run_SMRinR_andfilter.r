8d1e690417026b10c5c67a3dc34f65293e0b34c1
Lulu Shi <crazzy_rabbit@163.com>
2024-10-21 16:39:18 +0800

    Update run_SMRinR_andfilter.r

--- SMR & HEIDI analysis/run_SMRinR_andfilter.r
+++ SMR & HEIDI analysis/run_SMRinR_andfilter.r
@@ -1,4 +1,5 @@
 library(dplyr)
+library(optparse)
 library(data.table)
 # r function run smr qtl to trait
 run_smr_qtl <- function(gwas, qtl, outname){
@@ -18,15 +19,48 @@ run_smr_qtl <- function(gwas, qtl, outname){
 # p_SMR & p_HEIDI filter
 run_filter <- function(smr, outname){
     smr_file = fread(smr)
+    smr_tsh = sprintf("%.2e", 0.05/nrow(smr_file))
     flt_file = smr_file %>% filter(p_SMR <= 0.05/n() & p_HEIDI >= 0.01)
 
-    write.table(flt_file, file=paste0(outname, "_", sprintf("%.2e", 0.05/nrow(smr_file)), "smr_0.01heidi.txt"), sep="\t", row.names=FALSE, quote=FALSE)
+    write.table(flt_file, file=paste0(outname, "_", smr_tsh, "smr_0.01heidi.txt"), sep="\t", row.names=FALSE, quote=FALSE)
 }
 
-args <- commandArgs(TRUE)
-gwas <- args[which(args == "--gwas") + 1]
-qtl  <- args[which(args == "--qtl") + 1]
-out  <- args[which(args == "--out")  + 1]
+# args <- commandArgs(TRUE)
+# gwas <- args[which(args == "--gwas") + 1]
+# qtl  <- args[which(args == "--qtl") + 1]
+# out  <- args[which(args == "--out")  + 1]
+# func <- ifelse("--func" %in% args, args[which(args == "--func") + 1], "all")
 
-run_smr_qtl(gwas, qtl, out)
-runfilter(paste0(out, ".smr"), out)
+option_list <- list(
+    make_option(c("--func", type="character"), default="both", help="Function to run: 'run_smr_qtl', 'run_filter', or 'both'"),
+    make_option(c("--gwas"), type="character", help="GWAS summary data to run smr"),
+    make_option(c("--qtl"), type="character", help="qtl file to run smr"),
+    make_option(c("--out"), type="character", help="out prefix"),
+    make_option(c("--smr"), type="character", help="SMR file for filtering")
+)
+opt_parser <- OptionParser(option_list=option_list)
+opts <- parse_args(opt_parser)
+
+gwas <- opts$gwas
+qtl  <- opts$qtl
+out  <- opts$out
+func <- opts$func
+smr  <- opts$smr
+
+if (func == "run_smr_qtl"){
+    if (is.null(gwas) || is.null(qtl)) {
+        stop("When func is 'run_smr_qtl', --gwas and --eqtl must be provided.")
+    }
+    run_smr_qtl(gwas, qtl, out)
+} else if (func == "run_filter"){
+    if (is.null(smr)) {
+        stop("When func is 'run_filter', --smr must be provided")
+    }
+    run_filter(smr, out)
+} else{
+    if (is.null(gwas) || is.null(qtl)) {
+        stop("When func is not provided, --gwas and --qtl must be provided.")
+    }
+    run_smr_qtl(gwas, qtl, out)
+    run_filter(paste0(out, ".smr"), out)
+}
